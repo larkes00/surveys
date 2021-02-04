ea6eb6f187c4b4c428a4b6abe4704bdacb8ce948
Oleh Zakirov <zakirov2802@gmail.com>
2021-02-04 18:52:17 +0200

    Test logout

--- surveys/tests/test_views/test_logout.py
+++ surveys/tests/test_views/test_logout.py
@@ -1,6 +1,8 @@
-from surveys.tests.test_views.helpers import make_session, make_user
-import pytest
 from django import urls
+import pytest
+
+from surveys.tests.test_views.helpers import make_session
+from surveys.tests.test_views.helpers import make_user
 
 
 def setup():
@@ -8,21 +10,31 @@ def setup():
     make_session()
 
 
+def get_logout_url():
+    return urls.reverse("logout")
+
+
+@pytest.mark.django_db
+def test_logout_only_post(client):
+    response = client.get(get_logout_url())
+    assert response.status_code == 405
+
+
 @pytest.mark.django_db
 def test_successful_logout(client):
-    url = urls.reverse("logout")
     response = client.post(
-        url,
+        get_logout_url(),
         {"session_id": "c101a895-f2c0-43a9-ac3e-a54f7f334d56"},
         content_type="application/json",
-    )  # fmt: on
+    )
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_unsuccessful_logout(client):
-    url = urls.reverse("logout")
-    response = client.post(
-        url, {"session_id": "21daxz"}, content_type="application/json"
-    )
+    response = client.post(  # fmt off
+        get_logout_url(),
+        {"session_id": "21daxz"},
+        content_type="application/json",
+    )  # fmt on
     assert response.status_code == 400
