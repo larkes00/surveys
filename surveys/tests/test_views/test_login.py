66eb0bb2f9f8943be0a81c45394827c5bed41873
Oleh Zakirov <zakirov2802@gmail.com>
2021-02-04 18:40:55 +0200

    Test login

--- surveys/tests/test_views/test_login.py
+++ surveys/tests/test_views/test_login.py
@@ -1,35 +1,47 @@
-from surveys.tests.test_views.helpers import make_user
-import pytest
 from django import urls
+import pytest
+
+from surveys.tests.test_views.helpers import make_user
 
 
-def setup_function():
+def setup():
     make_user()
+
+
+def get_login_url():
+    return urls.reverse("login")
+
+
+@pytest.mark.django_db
+def test_login_only_post(client):
+    response = client.get(get_login_url())
+    assert response.status_code == 405
 
 
 @pytest.mark.django_db
 def test_succesful_login(client):
-    url = urls.reverse("login")
     response = client.post(
-        url, {"login": "Bad12345", "password": "12345"}, content_type="application/json"
+        get_login_url(),
+        {"login": "Bad12345", "password": "12345"},
+        content_type="application/json",
     )
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_login_wrong_login(client):
-    url = urls.reverse("login")
+def test_wrong_login(client):
     response = client.post(
-        url, {"login": "-1", "password": "qq"}, content_type="application/json"
+        get_login_url(),
+        {"login": "-1", "password": "qq"},
+        content_type="application/json",
     )
     assert response.status_code == 404
 
 
 @pytest.mark.django_db
 def test_login_wrong_password(client):
-    url = urls.reverse("login")
     response = client.post(
-        url,
+        get_login_url(),
         {"login": "Bad12345", "password": "zzzzzz"},
         content_type="application/json",
     )
