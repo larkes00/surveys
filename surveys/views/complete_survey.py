085a609259c256f40aad03f5c35ea0bde66f62b1
Oleh Zakirov <zakirov2802@gmail.com>
2021-03-16 17:41:14 +0200

    Rework complete_survey

--- surveys/views/complete_survey.py
+++ surveys/views/complete_survey.py
@@ -1,14 +1,13 @@
 import datetime
 import json
-from json import JSONDecodeError
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.http.response import HttpResponseBadRequest
 
-from surveys.logic import allow_only, validate
-from surveys.models import CompleteSurvey, CompleteSurveyQuestion
-from surveys.serializers import CompleteSurveySerializer
+from surveys.logic import allow_only
+from surveys.logic import validate
+from surveys.models import CompleteSurvey
+from surveys.models import CompleteSurveyQuestion
 from surveys.settings import URL_LOGIN_REDIRECT
 
 
@@ -16,25 +15,25 @@ from surveys.settings import URL_LOGIN_REDIRECT
 @login_required(login_url=URL_LOGIN_REDIRECT)
 # @validate(CompleteSurveySerializer)
 def new_complete_survey(request):
-    complete_surveys = json.loads(request.body)
+    complete_surveys_json = json.loads(request.body)
     questions = []
-    for obj in complete_surveys["questions"]:
+    complete_survey = CompleteSurvey(
+        user_id=complete_surveys_json["user_id"],
+        survey_id=complete_surveys_json["survey_id"],
+        completed_at=datetime.datetime.utcnow(),
+    )
+    complete_survey.save()
+    for obj in complete_surveys_json["questions"]:
         questions.append(
             {
                 "question_id": obj["question_id"],
                 "answer_id": obj["answer_id"],
-                "complete_survey": obj["complete_survey"],
             }
         )
     for question in questions:
         CompleteSurveyQuestion(
             question_id=question["question_id"],
             answer_id=question["answer_id"],
-            complete_survey_id=question["complete_survey"],
+            complete_survey_id=complete_survey.id,
         ).save()
-    CompleteSurvey(
-        user_id=complete_surveys["user_id"],
-        survey_id=complete_surveys["survey_id"],
-        completed_at=datetime.datetime.utcnow(),
-    ).save()
-    return JsonResponse({"data": complete_surveys})
+    return JsonResponse({"data": complete_surveys_json})
